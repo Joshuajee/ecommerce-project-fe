a905a47b530f92bc6a3f8154798c4858075a7030
Favour <favourOL1@outlook.com>
2024-04-11 10:53:37 +0100

    fav

--- js/shop.js
+++ js/shop.js
@@ -30,6 +30,36 @@ const products = [
         name: "XIAOMI Redmi A3 3GB RAM/64GB ROM ",
         price: 100000,
         image: BASE_URL+ "1 (2).jpg", 
+    },
+    {
+        id: 6,
+        name: "infinix Hot 5 pro 8gb RAM + 128gb ROM ",
+        price: 80000,
+        image: BASE_URL+ "infinix.jpg", 
+    },
+    {
+        id: 7,
+        name: "OPPO Hot 40T 3gb RAM + 64gb ROM",
+        price: 100000,
+        image: BASE_URL+ "infinix.jpg", 
+    },
+    {
+        id: 8,
+        name: "LG Computer Entertainment Sony PS Vita (WiFi) Console",
+        price: 50000,
+        image: BASE_URL+ "1(3).jpg", 
+    },
+    {
+        id: 9,
+        name: "hisense 43' Inches LED Smart TV ....",
+        price: 100000,
+        image: BASE_URL+ "1.jpg", 
+    },
+    {
+        id: 10,
+        name: " Samsung A3 3GB RAM/64GB ROM ",
+        price: 100000,
+        image: BASE_URL+ "1 (2).jpg", 
     }
 ]
 
@@ -47,11 +77,15 @@ function addToCart(id){
 
     sessionStorage.setItem("items",JSON.stringify(items))
 
+    cartCount()
+
     alert(product.name + ' added to cart')
 
 }
 
-
+function cartCount(){
+    document.getElementById("cart-count").innerHTML=getCart().length
+}
 
 
 
@@ -65,3 +99,6 @@ function getCart(){
     }
 
 }
+
+
+cartCount()
